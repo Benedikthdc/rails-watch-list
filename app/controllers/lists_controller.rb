5af9a52ca37ba8310e21d0e7a24c68241ca5ffa3
benedikt conze <benedikt@conze.de>
2022-05-20 10:36:05 +0200

    done with setup

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,38 @@
+class ListsController < ApplicationController
+  def index
+    if params[:query].present?
+      @lists = List.where("name LIKE ?", "%#{params[:query]}%")
+    else
+      @lists = List.all
+    end
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :picture_url)
+  end
+end
